21a0dcf0f77c69155f43ef371dad976bb5d0c8cc
ngay <nathanaelwgay@netscape.net>
2023-03-15 16:12:36 -0400

    Testing R file writing

---
+++ R/ds1.R
@@ -0,0 +1,16 @@
+#' 14th Edition Triola Data Set
+#' 
+#' Consult data set descriptions in the textbook Appendix B, starting on page 749.
+#' 
+#' @format A data frame with 180,400 rows and 1 variable.
+#' @source Triola, M. 2022. Elementary Statistics, 14th ed. Boston, MA: Pearson.
+#' @name ds1
+NULL
+#' 14th Edition Triola Data Set
+#' 
+#' Consult data set descriptions in the textbook Appendix B, starting on page 749.
+#' 
+#' @format A data frame.
+#' @source Triola, M. 2022. Elementary Statistics, 14th ed. Boston, MA: Pearson.
+#' @name ds1
+NULL
